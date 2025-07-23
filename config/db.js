f70c0fe1fa29551702c903bb40e736013b6a4c99
Rajesh-zha <Zharazesh89@gmail.com>
2025-07-24 00:09:54 +0545

    Fix delete user function logging

--- config/db.js
+++ config/db.js
@@ -21,7 +21,7 @@ export async function connectDB() {
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
       bufferCommands: false,
-        dbName: "MeroCart"
+        // dbName: "MeroCart"
     });
   }
 
